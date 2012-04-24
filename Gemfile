e6aceb51c841fd1b72c96e1f2d10fdc53b27e3d1
soupdawgie <soupdawgie@gmail.com>
2012-04-24 20:16:14 +0300

    Revert "Make gemfile changes"
    This reverts commit 7a534cd0c281da639336f6c0a1abced8568bee09.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source :gemcutter
 
 gem 'rails', '2.3.5'
 gem 'rake', '0.8.7'
-gem 'heroku'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -23,7 +23,6 @@ class CampaignsController < ApplicationController
   def create
 		@campaign = Campaign.new(params[:campaign])
 		@planning = @campaign.build_planning(params[:planning])
-		
 		if @campaign.save
 			flash[:success] = "New campaign was successfully created!"
 			redirect_to root_path
@@ -46,7 +45,6 @@ class CampaignsController < ApplicationController
   def update
 		@campaign = Campaign.find(params[:id])
 		@planning = @campaign.planning
-		
 		if @campaign.update_attributes(params[:campaign]) &&
 		(@planning.update_attributes(params[:planning]) if !@campaign.planning.nil?)
 			flash[:success] = "Campaign information was successfully updated."
